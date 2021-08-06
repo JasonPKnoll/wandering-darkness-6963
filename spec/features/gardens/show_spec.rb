eec25412b0d687f66af4befe8710a75ee247d89a
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-06 09:06:04 -0700

    Add tests for garden show spec

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Plot do
-  describe 'plots index page' do
+RSpec.describe Garden do
+  describe 'gardens show page' do
     before(:each) do
       @garden = Garden.create!(name: "Turing Comunity Garden", organic: true)
+      @garden_2 = Garden.create!(name: "Desert Garden", organic: false)
       @plot_1 = @garden.plots.create!(number: 25, size: "Large", direction: "East")
       @plot_2 = @garden.plots.create!(number: 20, size: "Medium", direction: "Weast")
       @plot_3 = @garden.plots.create!(number: 15, size: "Small", direction: "Southish")
+      @plot_4 = @garden.plots.create!(number: 15, size: "Small", direction: "Southish")
+
 
       @plant_1 = Plant.create!(name: "Purple Beauty Sweet Bell Pepper", description: "Prefers  rich, well draining soil", days_to_harvest: 90)
       @plant_2 = Plant.create!(name: "White Lotus", description: "Extreamly Rare", days_to_harvest: 365)
       @plant_3 = Plant.create!(name: "Spikey plant of def", description: "Prefers to be left alone", days_to_harvest: 30)
       @plant_4 = Plant.create!(name: "Potato", description: "Prefers straight up dirt", days_to_harvest: 60)
       @plant_5 = Plant.create!(name: "Amaranth", description: "A purple grain cultivated by an ancient civilization", days_to_harvest: 7)
+      @plant_6 = Plant.create!(name: "Sweet Gem Berry", description: "It's by far the sweetest thing you've ever smelled.", days_to_harvest: 24)
+
 
       Assignment.create!(plot: @plot_1, plant: @plant_1)
       Assignment.create!(plot: @plot_1, plant: @plant_4)
@@ -20,42 +25,23 @@ RSpec.describe Plot do
       Assignment.create!(plot: @plot_2, plant: @plant_2)
       Assignment.create!(plot: @plot_2, plant: @plant_5)
       Assignment.create!(plot: @plot_3, plant: @plant_4)
+      Assignment.create!(plot: @plot_4, plant: @plant_6)
 
-      visit "/plots"
+      visit "/gardens/#{@garden.id}"
     end
 
-    it 'shows list of all plot numbers' do
-      expect(page).to have_content("25")
-      expect(page).to have_content("20")
-      expect(page).to have_content("15")
+    it "can visit the garden" do
+      expect(current_path).to eq(garden_path)
     end
 
-    it 'shows the name of each plots plants under numbers' do
-      # within "plot-#{@plot_1.number}" do
-        expect(page).to have_content("#{@plant_1.name}")
-        expect(page).to have_content("#{@plant_4.name}")
-        expect(page).to have_content("#{@plant_3.name}")
-      # end
-      # within "plot-#{@plot_2.number}" do
-        expect(page).to have_content("#{@plant_2.name}")
-        expect(page).to have_content("#{@plant_5.name}")
-      # end
-      # within "plot-#{@plot_3.number}" do
-        expect(page).to have_content("#{@plant_4.name}")
-      # end
-    end
+    it 'shows a list of all plants in this garden' do
+      expect(page).to have_content(@plant_1.name)
+      expect(page).to have_content(@plant_2.name)
+      expect(page).to have_content(@plant_3.name)
+      expect(page).to have_content(@plant_4.name)
+      expect(page).to have_content(@plant_5.name)
 
-    it "see link to reomve that plant from plot" do
-      click_link "Remove #{@plant_5.name}"
-    end
-
-    it "can remove a plant from a plot bbut not delete plant" do
-      click_link "Remove #{@plant_5.name}"
-
-      expect(current_path).to eq("/plots")
-
-      expect(page).to_not have_content("#{@plant_5.name}")
-      expect(Plant.fifth).to eq(@plant_5)
+      expect(page).to_not have_content(@plant_6.name)
     end
   end
 end
