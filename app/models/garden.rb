86f558264e9fa83c471b4f143f230ff32ea52b07
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-06 09:36:11 -0700

    Complete iteration 3

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,11 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+  def all_plants
+    plots.joins(:plants)
+          .select("plants.name")
+          .group(:name)
+          .where("plants.days_to_harvest < ?", 100)
+          .pluck("plants.name")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  resources :gardens, only: :show
   resources :plots, only: :index
-  resources :plants
+  resources :plants, only: :destroy
 
 end
